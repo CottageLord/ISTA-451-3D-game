db6575ddd5314b974a26fbef079039578ce7966d
Yang Hu <gymf1010@email.arizona.edu>
2018-10-16 13:46:02 -0700

    basic player controller
    basic movement and jump

--- ISTA451 3D game/Assets/Scripts/Player/playerMovement.cs
+++ ISTA451 3D game/Assets/Scripts/Player/playerMovement.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public float speed;
+	public float jumpPower;
+	public float gravity = 40f;
+    public Rigidbody rb;
+    public CharacterController controller;
+    /*
+    void Update() {
+    	float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = Vector3.zero;
+        if(controller.isGrounded) {
+        	movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        	// convert move direction from local to world space
+        	movement = transform.TransformDirection(movement);
+        	movement *= speed;
+        	if(Input.GetButtonDown("Jump")) {
+	        	movement.y = jumpPower;
+        	}
+        }
+        movement.y -= gravity * Time.deltaTime;
+
+        controller.Move(movement * Time.deltaTime);
+    }
+	*/
+    
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        // move on ground
+        rb.velocity = new Vector3(moveHorizontal * speed, 
+        	rb.velocity.y, moveVertical * speed);
+
+        //transform.Rotation();
+
+        if(Input.GetButtonDown("Jump")) {
+        	rb.velocity = new Vector3(rb.velocity.x, 
+        		jumpPower, rb.velocity.z);
+        }
+    }
 }
