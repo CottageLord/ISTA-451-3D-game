b69d62fc80212ab46b33df9776d07c737a0c5ec4
Yang Hu <gymf1010@email.arizona.edu>
2018-10-15 21:14:31 -0700

    added assets folders and script skeleton
    rule script will include all of the const rules. ie: player health, speed, map size...

---
+++ ISTA451 3D game/Assets/Scripts/rule.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rule : MonoBehaviour {
+	public const int PLAYER_MAX_HEALTH;
+	public const int PLAYER_SPEED;
+}
