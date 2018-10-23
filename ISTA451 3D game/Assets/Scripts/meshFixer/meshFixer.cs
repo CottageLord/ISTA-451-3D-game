0dd343f71a7c04854f57a502ea458da8678459a0
Yang Hu <gymf1010@email.arizona.edu>
2018-10-23 01:22:55 -0700

    added basic GUI system, added basic object interactions
    when player approach an object, a text will be displayed.

---
+++ ISTA451 3D game/Assets/Scripts/meshFixer/meshFixer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshFixer : MonoBehaviour {
+
+	void Start() {
+		GetComponent<MeshFilter>().mesh.UploadMeshData(true);
+	}
+}
