306ee591b48ccf9091e04bb686a61fd97d8416d0
Yang Hu <gymf1010@email.arizona.edu>
2018-10-26 00:14:52 -0700

    updated rock floating effect

---
+++ ISTA451 3D game/Assets/Scripts/interaction/level pass/floatObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatObj : MonoBehaviour {
+	public Transform self;
+	public float speed;
+	private int finalHeight = 30;
+	private bool floating = false;
+	// Update is called once per frame
+	void Update() {
+		if(floating) {
+			self.position += new Vector3(0f, speed, 0f);
+		}
+	}
+
+	public void floatUp() {
+		floating=true;
+		StartCoroutine(floatUpStart());
+	}
+	
+
+	IEnumerator floatUpStart() {
+		yield return new WaitForSeconds(10);
+		Destroy(self.gameObject);
+	}
+}
--- ISTA451 3D game/Assets/Scripts/interaction/level pass/passLevel.cs
+++ ISTA451 3D game/Assets/Scripts/interaction/level pass/passLevel.cs
@@ -3,22 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class passLevel : MonoBehaviour {
-	public Rigidbody[] rocks;
+	public floatObj[] rocks;
 	public GameObject invisWalls;
 	public GameObject allRocks;
 
 	public void pass() {
 		invisWalls.SetActive(false);
-		StartCoroutine(floatRocks());
-	}
-
-	IEnumerator floatRocks() {
-		foreach(Rigidbody rock in rocks) {
-			rock.AddForce(0,10,0);
+		foreach(floatObj rock in rocks) {
+			rock.floatUp();
 		}
-
-		yield return new WaitForSeconds(3);
-
-		Destroy(allRocks);
 	}
 }
